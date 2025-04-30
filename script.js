7c2b71718fe4f5e7ee4218b2dd0dc49ca95748ca
dang2000sky <ngonguyenminhdang2010@gmail.com>
2025-04-30 15:39:33 +0700

    80%

--- script.js
+++ script.js
@@ -2,18 +2,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const stopButton = document.getElementById("stopBtn");
     const star = document.querySelector(".gift__bow");
-    const gift1 = document.querySelector(".gift__bow-left");
-    const gift2 = document.querySelector(".gift__bow-right");
     const gift = document.querySelector(".gift__lid");
-    const gift3 = document.querySelector(".gift__lid-shadow");
     const gift4 = document.querySelector(".gift__box");
+
+    let hasb = false
+    function nhan(){
+        
+    }
     // Khi nhấn nút "Dừng hiệu ứng"
     stopButton.addEventListener("click", function() {
-        gift4.style.animationPlayState = "paused";
-        star.style.animationPlayState = "paused"; // Dừng hiệu ứng
-        gift1.style.animationPlayState = "paused"; // Dừng hiệu ứng
-        gift2.style.animationPlayState = "paused"; // Dừng hiệu ứng
-        gift.style.animationPlayState = "paused"; // Dừng hiệu ứng
-        gift3.style.animationPlayState = "paused"; // Dừng hiệu ứng
+        gift4.style.animation = "openhop 4s forwards";
+        star.style.animation = "no 4.07s forwards"; // Dừng hiệu ứng
+        gift.style.animation = "opennap 4s forwards"; // Dừng hiệu ứng
     });
 });
