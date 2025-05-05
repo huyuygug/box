f4e6c0de0ae34f1c875c551349cc49ceef74bf6f
dang2000jack <ngonguyenminhdang2010@gmail.com>
2025-05-06 00:42:59 +0800

    hoan thanh text

--- script.js
+++ script.js
@@ -4,15 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const star = document.querySelector(".gift__bow");
     const gift = document.querySelector(".gift__lid");
     const gift4 = document.querySelector(".gift__box");
+    const text = document.querySelector(".text");
 
-    let hasb = false
-    function nhan(){
-        
-    }
+
     // Khi nhấn nút "Dừng hiệu ứng"
     stopButton.addEventListener("click", function() {
-        gift4.style.animation = "openhop 1s forwards";
+        gift4.style.animation = "openhop 5s forwards";
         star.style.animation = "no 1s forwards"; // Dừng hiệu ứng
         gift.style.animation = "opennap 1s forwards"; // Dừng hiệu ứng
+        text.style.animation = "ease-in-out 5s xuathien forwards"; // Dừng hiệu ứng
     });
 });
